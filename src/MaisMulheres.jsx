ccea56aaddf247e56033af3cb538e5a7becea26e
pedroccarv <124697055+pedroccarv@users.noreply.github.com>
2024-08-27 07:43:22 -0300

    Update MaisMulheres.jsx

--- src/MaisMulheres.jsx
+++ src/MaisMulheres.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import dados from '/api.json';
+import dados from './api.json';
 import { Link } from 'react-router-dom';
 function Mulheres() {
   const [tenis, setTenis] = useState([]);
