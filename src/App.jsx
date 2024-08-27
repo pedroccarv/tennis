dc263b7a8092caf7dc047393ca9cca73b315ab20
pedroccarv <124697055+pedroccarv@users.noreply.github.com>
2024-08-27 07:46:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { Link } from 'react-router-dom';
-import dados from './api.json';
+import dados from '/api.json';
 
 function App() {
 
